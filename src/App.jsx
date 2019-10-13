af8dd513b2ff3300fe24e7ecd72ce79d22f36230
jamiebdavis <jamied1985@gmail.com>
2019-10-13 17:57:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My App</h1>
+    </div>
+  );
+}
+
+export default App;

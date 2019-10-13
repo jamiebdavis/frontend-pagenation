efd0f9e77f03df35097a3ff2c0ec35aad988cb2f
jamiebdavis <jamied1985@gmail.com>
2019-10-13 21:23:33 +0100

    striped back react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "./App.css";
+import { async } from "q";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+      setPosts(res.data);
+      setLoading(false);
+    };
+  });
+
   return (
     <div className="container">
       <h1>My App</h1>
